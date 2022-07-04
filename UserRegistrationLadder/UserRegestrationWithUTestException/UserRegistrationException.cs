f2ff478198f42d8d6816a44cb53dbf4376499391
Luv sahu <lksahu404@gmail.com>
2022-07-04 17:11:41 +0530

    [Luv SAHU] Lambda Function Validation For user

---
+++ UserRegistrationLadder/UserRegestrationWithUTestException/UserRegistrationException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegestrationWithUTestException
+{
+    public class UserRegistrationException : Exception
+    {
+
+        public enum ExceptionType
+        {
+            EMPTY_FIRSTNAME,
+            INVALID_FIRSTNAME,
+            //EMPTY_LASTNAME,
+            INVALID_LASTNAME,
+            //EMPTY_EMAIL_ID,
+            INVALID_EmailId,
+            //EMPTY_PASSWORD,
+            INVALID_PASSWORD,
+            //EMPTY_MOBILE_NO,
+            INVALID_MOBILE_NO
+        }
+        private readonly ExceptionType type;
+
+            public UserRegistrationException(ExceptionType Type, String message) : base(message)
+            {
+                this.type = Type;
+            }
+        }
+}
+
